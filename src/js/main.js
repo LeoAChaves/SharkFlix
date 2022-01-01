d368fb953b6ae4fa16eebec7bde0c3e8012f2509
FilpeRibeiro <filipe-negreti@hotmail.com>
2021-12-31 23:44:03 -0300

    trailers adicionados responsivo ok fechar video com o model corrigir

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+
+// $('#fechar').click(function() {
+//     $('#videoT').each(function(){ 
+//         let frame = document.getElementById("videoT");
+//         frame.contentWindow.postMessage(
+//             '{"event":"command","func":"pauseVideo","args":""}',
+//             '*'); 
+//     });
+// });
+
