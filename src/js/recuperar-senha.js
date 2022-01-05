b8e10b88eff3255c6c2573f253aaca2abc0a5b98
wandbergdelima <wandberg@gmail.com>
2022-01-05 00:06:47 -0300

    Paginas de login e recuperação de Senha concluidas

---
+++ src/js/recuperar-senha.js
@@ -0,0 +1,34 @@
+// Função do botão Salvar
+
+let listaMail = [];
+let listaPass = [];
+let listaRepass = [];
+
+$('.salvar').click((e)=>{
+    e.preventDefault();    
+    let mail = $('#email').val()
+    let pass = $('#pass').val()
+    let repass = $('#re-pass').val()
+    let envio = $('.textorecupera')
+    listaMail.push(mail);
+    listaPass.push(pass);
+    listaRepass.push(repass);
+    if (mail != "") {
+        if (pass === repass && pass != "" && repass != "") {
+                envio.html('Email Enviado com sucesso!')
+                envio.css('color', 'green')
+                envio.css('border', '1px solid green')
+                envio.css('border-radius', '5px')
+        }else{
+            envio.html('Senhas Diferentes')
+            envio.css('color', 'red')
+                envio.css('border', '1px solid red')
+                envio.css('border-radius', '5px')
+        }
+    }else{
+        envio.html('Digite um Email Valido')
+        envio.css('color', 'red')
+                envio.css('border', '1px solid red')
+                envio.css('border-radius', '5px')
+    }
+})
