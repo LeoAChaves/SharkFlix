6591ccf22e7bba8de8d43768d73d1f3e9ff71608
LeoAChaves <leochaves2121@gmail.com>
2022-01-07 01:30:54 -0300

    limpando código página cadastro

--- src/js/view/cadastroView.js
+++ src/js/view/cadastroView.js
@@ -1,34 +1,22 @@
 class CadastroView {
-
-    verEmail(resp) {
-
-        if (resp == true) {
-            $('#emailCorreto').text("")
-        } else {
-            $('#emailCorreto').text("E-mail Incorreto!")
-        }
-
+  verEmail(resp) {
+    if (resp == true) {
+      $("#emailCorreto").text("");
+    } else {
+      $("#emailCorreto").text("E-mail Incorreto!");
     }
-
-    verSenha(resp) {
-
-        if (resp == true) {
-
-            $('#senhaCorreta').text("")
-
-        } else {
-
-            $('#senhaCorreta').text("A senha devem ser iguais!")
-        }
+  }
+  verSenha(resp) {
+    if (resp == true) {
+      $("#senhaCorreta").text("");
+    } else {
+      $("#senhaCorreta").text("A senha devem ser iguais!");
     }
-
-    mostrarDados(resposta) {
-
-        $('#rua').val(resposta.logradouro)
-        $('#bairro').val(resposta.bairro)
-        $('#cidade').val(resposta.localidade)
-        $('#estado').val(resposta.uf)
-
-    }
-
+  }
+  mostrarDados(resposta) {
+    $("#rua").val(resposta.logradouro);
+    $("#bairro").val(resposta.bairro);
+    $("#cidade").val(resposta.localidade);
+    $("#estado").val(resposta.uf);
+  }
 }
