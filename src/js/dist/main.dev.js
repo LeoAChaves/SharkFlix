b66a1d96a1de54ba0f8e4113ebe1e85c32bcf231
LeoAChaves <leochaves2121@gmail.com>
2022-01-07 02:00:21 -0300

    limpando e identando código -login/recuperarsenha

--- src/js/dist/main.dev.js
+++ src/js/dist/main.dev.js
@@ -1,2 +1,1 @@
-// const controller = new PesquisaController();
 "use strict";
--- src/js/login.js
+++ src/js/login.js
@@ -1,27 +1,25 @@
-// Função do botão Login
-
-$('.login').click((e)=>{
-    e.preventDefault();    
-    let mail = $('#email').val()
-    let pass = $('#pass').val()
-    let envio = $('.textorecupera')
-    if (mail != "") {
-        if (pass != "") {
-            envio.html('Login efetuado com sucesso!')
-            envio.css('color', 'green')
-            envio.css('border', '1px solid green')
-            envio.css('border-radius', '5px')
-            location.replace((URL="../index.html"))
-        }else{
-            envio.html('Senha Invalida')
-            envio.css('color', 'red')
-            envio.css('border', '1px solid red')
-            envio.css('border-radius', '5px')
-        }
-    }else{
-        envio.html('Digite um Login Valido')
-        envio.css('color', 'red')
-        envio.css('border', '1px solid red')
-        envio.css('border-radius', '5px')
+$(".login").click((e) => {
+  e.preventDefault();
+  let mail = $("#email").val();
+  let pass = $("#pass").val();
+  let envio = $(".textorecupera");
+  if (mail != "") {
+    if (pass != "") {
+      envio.html("Login efetuado com sucesso!");
+      envio.css("color", "green");
+      envio.css("border", "1px solid green");
+      envio.css("border-radius", "5px");
+      location.replace((URL = "../index.html"));
+    } else {
+      envio.html("Senha Invalida");
+      envio.css("color", "red");
+      envio.css("border", "1px solid red");
+      envio.css("border-radius", "5px");
     }
-})
+  } else {
+    envio.html("Digite um Login Valido");
+    envio.css("color", "red");
+    envio.css("border", "1px solid red");
+    envio.css("border-radius", "5px");
+  }
+});
