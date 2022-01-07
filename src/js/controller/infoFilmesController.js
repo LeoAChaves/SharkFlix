8e4a69e5b376900c5ca619c3e6f85f97ba498ee9
LeoAChaves <leochaves2121@gmail.com>
2022-01-07 20:44:50 -0300

    correções pagina de info filmes

--- src/js/controller/infoFilmesController.js
+++ src/js/controller/infoFilmesController.js
@@ -1,5 +1,5 @@
 async function requisicaoInfoFilmes(id) {
   return await $.ajax({
-    url: "http://www.omdbapi.com/?i=" + id + "&apikey=3168fe11",
+    url: "https://www.omdbapi.com/?i=" + id + "&apikey=3168fe11",
   });
 }
